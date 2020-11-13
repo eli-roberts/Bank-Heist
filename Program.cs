b252fb0423093eb7cf4fbac79d56b7923f75173a
Eli Lavoie <storey14@googlemail.com>
2020-11-13 13:08:09 -0600

    phase two completed

--- Program.cs
+++ Program.cs
@@ -6,16 +6,71 @@ namespace bankHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan your heist!");
-            Console.WriteLine("What is the member's name?");
-            string codeName = Console.ReadLine();
-            Console.WriteLine($"What is {codeName}'s skill level?");
-            int skill = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"How courageous is {codeName}?");
-            double courage = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            //Initializes heist member list
+            var HeistTeam = new Team();
 
-            var memberOne = new HeistMember(codeName, skill, courage);
-            memberOne.description();
+            //Introductory console stuff
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine("What is your heist team's name?");
+            string teamName = Console.ReadLine();
+            HeistTeam.teamName = teamName;
+
+            bool quitting = false;
+            while(quitting != true)
+            {
+              Console.Clear();
+              Console.WriteLine($"1. Hire Member");
+              Console.WriteLine($"2. Check team.");
+              Console.WriteLine($"0. Quit Program");
+              string input = Console.ReadLine();
+              if(input == "1")
+              {
+                addMember(HeistTeam);
+              }
+              if(input == "2")
+              {
+                HeistTeam.checkTeam();
+              }
+              if(input == "0")
+              {
+                quitting = true;
+              }
+            }
+
+        }
+        static void addMember(Team team)
+        { 
+
+          //Init member attributes
+          string codeName = "";
+          int skill = 0;
+          double courage = 0.0;
+          
+          Console.Clear();
+
+          while (codeName == "")
+          {
+            codeName = getName();
+          }
+
+          Console.WriteLine($"What is {codeName}'s skill level?");
+          skill = Convert.ToInt32(Console.ReadLine());
+
+          Console.WriteLine($"How courageous is {codeName}?");
+          courage = Convert.ToDouble(Console.ReadLine());
+
+          var member = new HeistMember(codeName, skill, courage);
+
+
+          team.addToTeam(member);
+        }
+
+        static string getName()
+        {
+          Console.WriteLine("What is the member's name?");
+          string name = Console.ReadLine();
+          return name;
         }
     }
 }
