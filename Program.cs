a74f4258d470dc8c1798249c9bdf74901794df80
Eli Lavoie <storey14@googlemail.com>
2020-11-13 13:37:55 -0600

    completed phase three

--- Program.cs
+++ Program.cs
@@ -16,12 +16,20 @@ namespace bankHeist
             string teamName = Console.ReadLine();
             HeistTeam.teamName = teamName;
 
+            int bankDiff = 100;
+            int teamSkill = 0;
+
             bool quitting = false;
             while(quitting != true)
             {
               Console.Clear();
+              foreach(var member in HeistTeam.MemberList)
+              {
+                teamSkill += member.SkillLevel;
+              }
               Console.WriteLine($"1. Hire Member");
               Console.WriteLine($"2. Check team.");
+              Console.WriteLine($"3. Run Heist Simulation");
               Console.WriteLine($"0. Quit Program");
               string input = Console.ReadLine();
               if(input == "1")
@@ -31,6 +39,10 @@ namespace bankHeist
               if(input == "2")
               {
                 HeistTeam.checkTeam();
+              }
+              if(input == "3")
+              {
+                runSimulation(teamSkill, bankDiff);
               }
               if(input == "0")
               {
@@ -54,8 +66,9 @@ namespace bankHeist
             codeName = getName();
           }
 
-          Console.WriteLine($"What is {codeName}'s skill level?");
-          skill = Convert.ToInt32(Console.ReadLine());
+          while (skill <= 0){
+            skill = getSkill();
+          }
 
           Console.WriteLine($"How courageous is {codeName}?");
           courage = Convert.ToDouble(Console.ReadLine());
@@ -71,6 +84,55 @@ namespace bankHeist
           Console.WriteLine("What is the member's name?");
           string name = Console.ReadLine();
           return name;
+        }
+        static int getSkill()
+        {
+          Console.WriteLine("What is the team member's skill level?");
+          int skill = Convert.ToInt32(Console.ReadLine());
+          if(skill <= 0)
+          {
+            Console.WriteLine("A team member cannot be negatively skilled!");
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+            getSkill();
+          }
+          return skill;
+        }
+
+        static double getCourage()
+        {
+          Console.WriteLine("How courageous is the team member?");
+          Console.WriteLine("(from 0.0 to 2.0)");
+          double courage = Convert.ToDouble(Console.ReadLine());
+          if(courage <= 0 || courage > 2.0)
+          {
+            Console.WriteLine("Courage must be greater than 0.0 and smaller than 2.0!");
+            Console.WriteLine("Press any key to try again!");
+            Console.ReadKey();
+            getCourage();
+          }
+          return courage;
+        }
+        static void runSimulation(int teamSkill, int bankDiff)
+        {
+          Console.Clear();
+          string dots = "............";
+          Console.Write("Running simulation");
+          foreach(var dot in dots)
+          {
+            Console.Write($"{dot}");
+            System.Threading.Thread.Sleep(450);
+          }
+          Console.Write("\nResult: ");
+          if(teamSkill > bankDiff)
+          {
+            Console.Write("Success");
+          }
+          else{
+            Console.Write("Failure");
+          }
+          Console.WriteLine("\nPress any key to continue...");
+          Console.ReadKey();
         }
     }
 }
